c0d33f94e127ec2c96da974ca99bf5f4de72032b
Jaylerth <oclean66@gmail.com>
2019-08-01 16:45:58 -0500

    database

--- config/database.php
+++ config/database.php
@@ -38,14 +38,14 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+       
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', ' us-cdbr-iron-east-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_fc23100195c022f'),
+            'username' => env('DB_USERNAME', 'bd82a346b2c84c'),
+            'password' => env('DB_PASSWORD', '41e59fcc'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
